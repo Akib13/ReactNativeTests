d10639a4a32081f445b3c94229129904d8222c31
Akib Ahmed <akibahmed@iut-dhaka.edu>
2023-01-06 23:24:19 +0200

    Camera implement (FAILED)

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -36,12 +36,14 @@ export default function Camera() {
                 ref={cameraRef}
                 type={RNCamera.Constants.Type.back}
                 style={styles.preview}
+                cameraId='1'
             >
                 <CustomButton
                     title="Capture"
                     color='#1eb900'
                     onPressFunction={() => captureHandle()}
                 />
+
             </RNCamera>
         </View>
     );
@@ -57,3 +59,77 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
     }
 });
+
+/*
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+
+export default class Camera extends PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          useCamera2Api={true}
+          cameraId="1"
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  takePicture = async () => {
+    console.log(this.camera);
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true, fixOrientation: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+*/
